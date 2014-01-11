65c55fed96535956c52a65d3fcf2951cc9cf21b9
 WenLong <wwlchuangye@sohu.com>
2014-01-11 14:54:06 +0800

    no message

---
+++ hello7.0x/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  hello7_0xAppController.h
+//  hello7.0x
+//
+//  Created by  WenLong on 13-11-5.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ hello7.0x/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  hello7_0xAppController.h
+//  hello7.0x
+//
+//  Created by  WenLong on 13-11-5.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
